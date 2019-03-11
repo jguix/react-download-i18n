9da23b63ae4f07cfe7ab787dbaa33a691a66dd6b
Juangui Jordán <juangui.jordan@mimacom.com>
2019-03-11 21:33:43 +0100

    Using data from phraseapp demo

--- example/src/App.js
+++ example/src/App.js
@@ -1,9 +1,11 @@
+// https://medium.freecodecamp.org/how-to-write-a-simple-react-search-plugin-publish-it-to-npm-and-deploy-it-to-github-pages-d8876dff7780
 import React, { Component } from 'react';
 import DownloadI18nService from 'react-download-i18n';
 
-const myToken = "c3b94567a19b117b219dbff853bf1c1e754abfe0f81b3dbc02c3fdafd9f237cf";
-const project_id = "0f1404d500f3f4b7ae12e17d8e943807";
-const locale_id = "es";
+// Some data from a demo project in phraseapp
+const i18n_api = "phraseapp";
+const token = "0000000000000000a45c4158e0858dd1";
+const project_id = "00000000000000004158e0858d2fa45c";
 const file_format = "i18next";
 
 export default class App extends Component {
@@ -13,29 +15,43 @@ export default class App extends Component {
     this.state = {};
   }
   componentDidMount() {
-    DownloadI18nService.init("phraseapp", myToken);
+    DownloadI18nService.init(i18n_api, token);
     DownloadI18nService.listLocales(project_id).then(locales => {
-      console.log(`Locales: ${JSON.stringify(locales)}`);
+      const localeNumber = locales.length;
       this.setState({locales});
+
+      let localeTranslations = {};
+      locales.map((locale) => {
+        DownloadI18nService.getLocaleTranslations(
+          project_id,
+          locale.name,
+          file_format
+        ).then(localeTranslation => {
+          localeTranslations[locale.name]=localeTranslation;
+          if (Object.keys(localeTranslations).length === localeNumber) {
+            this.setState({localeTranslations});
+          }
+        });
+      });
     });
 
-    DownloadI18nService.getLocale(project_id, locale_id).then(locale => {
-      console.log(`Locale details: ${JSON.stringify(locale)}`);
-      this.setState({locale});
-    });
+    // DownloadI18nService.getLocale(project_id, locale_id).then(locale => {
+    //   console.log(`Locale details: ${JSON.stringify(locale)}`);
+    //   this.setState({locale});
+    // });
 
-    DownloadI18nService.getLocaleTranslations(
-      project_id,
-      locale_id,
-      file_format
-    ).then(localeTranslations => {
-      console.log(`Locale translations: ${JSON.stringify(localeTranslations)}`);
-      this.setState({localeTranslations});
-    });
+    // DownloadI18nService.getLocaleTranslations(
+    //   project_id,
+    //   locale_id,
+    //   file_format
+    // ).then(localeTranslations => {
+    //   console.log(`Locale translations: ${JSON.stringify(localeTranslations)}`);
+    //   this.setState({localeTranslations});
+    // });
   }
 
   render() {
-    const { locales, locale, localeTranslations } = this.state;
+    const { locales, localeTranslations } = this.state;
     return (
       <div>
         <h1>React DownloadI18n</h1>
@@ -45,19 +61,22 @@ export default class App extends Component {
             { locales.map( locale => (<li key={locale.id}>{locale.name}</li>))}
           </div>          
         ) }
-        { locale && (
-          <div>
-            <p>Locale {locale.name}:</p>
-            <li>Created at: {locale.created_at}</li>
-            <li>Updated at: {locale.updated_at}</li>
-          </div>          
-        ) }
-        { locale && localeTranslations && (
-          <div>
-            <p>Locale {locale.name} translations</p>
-            { Object.keys(localeTranslations).map((translationKey) => { return (<li key={1000 * Math.random()}>{translationKey}: {localeTranslations[translationKey]}</li>) } )}
-          </div>
-        )}
+        { locales && localeTranslations && locales.map( locale => {
+          const localeTranslation = localeTranslations[locale.name];
+          return (
+            <div>
+              <div>
+                <p>Locale <b>{locale.name}</b>:</p>
+                <li>Created at: {locale.created_at}</li>
+                <li>Updated at: {locale.updated_at}</li>
+              </div>
+              <div>
+                <p>Locale <b>{locale.name}</b> translations: </p>
+                { Object.keys(localeTranslation).map((translationKey) => { return (<li key={1000 * Math.random()}>{translationKey}: {localeTranslation[translationKey]}</li>) } )}
+              </div>
+            </div>
+          );
+        }) }
       </div>
     )};    
   }
