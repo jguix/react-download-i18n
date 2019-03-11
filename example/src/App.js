b2b4780f0b4add701fc9e76d98e932b85bceaf42
Juangui Jordán <juangui.jordan@mimacom.com>
2019-03-11 02:47:02 +0100

    init create-react-library@2.6.7

---
+++ example/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import ExampleComponent from 'react-download-i18n'
+
+export default class App extends Component {
+  render () {
+    return (
+      <div>
+        <ExampleComponent text='Modern React component module' />
+      </div>
+    )
+  }
+}
